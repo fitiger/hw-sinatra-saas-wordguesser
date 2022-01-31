bdcbb2745bc8832fd7d689731261c6a305523873
Zhuoyuan Liu <liuzykl@gmail.com>
2022-01-31 16:01:09 +0100

    finished guess and show

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class WordGuesserApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    begin
+      valid = @game.guess(letter)
+      if valid == false
+        flash[:message] = "You have already used that letter."    
+      end
+    rescue
+      flash[:message] = "Invalid guess."
+    end
     redirect '/show'
   end
   
@@ -50,7 +58,14 @@ class WordGuesserApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state = @game.check_win_or_lose() 
+    if state == :win
+      redirect '/win'
+    elsif state == :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
